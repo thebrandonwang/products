a39a57e9715071f0ef2cae6a9c77cf2d4007daef
Brandon Wang <thebrandonwang@gmail.com>
2022-06-15 17:20:10 +0800

    add file checking system

--- products.py
+++ products.py
@@ -1,14 +1,20 @@
+import os #載入作業系統
 products = []
 
-# 讀取檔案，並把原有檔案加入products清單
-with open('product.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if 'product,price' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
+# 檢查有無 products.csv 檔案
+if os.path.isfile('products.csv'):
+	print('file found')
+	# 讀取檔案，並把原有檔案加入 products 清單
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if 'product,price' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+else:
+	print('file not found')
 
-# 請使用者輸入商品與價格並存到products清單
+# 請使用者輸入商品與價格並存到 products 清單
 while True:
 	name = input('please enter product name: ')
 	if name == 'q':
@@ -21,7 +27,7 @@ for p in products:
 	print('the price of', p[0], 'is', p[1])
 
 # 寫入商品價格
-with open('product.csv', 'w', encoding='utf-8') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('product,price\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
