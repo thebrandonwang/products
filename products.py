b5986c4b1db7ff6908dd765cb8f439bb03e25ccb
Brandon Wang <thebrandonwang@gmail.com>
2022-06-15 16:25:15 +0800

    v04, add categories

--- products.py
+++ products.py
@@ -7,6 +7,7 @@ while True:
 	products.append([name, price])
 for p in products:
 	print('the price of', p[0], 'is', p[1])
-with open('product.csv', 'w') as f:
+with open('product.csv', 'w', encoding='utf-8') as f:
+	f.write('product,price\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
